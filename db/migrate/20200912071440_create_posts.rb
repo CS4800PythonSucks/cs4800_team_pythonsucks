e4d46f2044de86597c5361f00d2cd78de0cada84
hlin91 <harveylin@cpp.edu>
2020-09-12 15:43:34 -0700

    Set sub_id to be unique and non-nil in Posts migrate file

--- db/migrate/20200912071440_create_posts.rb
+++ db/migrate/20200912071440_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :sub_id
+      t.string :sub_id, unique: true, null: false
       t.string :title
       t.string :url
       t.numeric :votes
