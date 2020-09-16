fbc27edbde77d5afc2d9069b6c669b4587e5d0b1
Ahuang0105 <alan240sx95@gmail.com>
2020-09-16 00:36:20 -0700

    Add page for alan

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,4 +2,9 @@ class AssignmentsController < ApplicationController
   def harvey
     @post = Post.order("RAND()").first
   end
+
+  def alan
+    @post = Post.order("RAND()").first
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Routes for assignment 3 part 3
   get '/assignments/harvey', to: 'assignments#harvey'
+  get '/assignments/alan', to: 'assignments#alan'
 end
