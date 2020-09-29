097633b3f062b1370bc58ea103484e59fba9f59f
hlin91 <hlin91@outlook.com>
2020-09-29 15:51:26 -0700

    Fixed caption for modal image

--- app/javascript/packs/post_index.js
+++ app/javascript/packs/post_index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    var title = document.getElementById("imgTitle")
     var modal = document.getElementById("homeModal");
     var img = document.getElementById("homeImg");
     var modalImg = document.getElementById("modalImg");
@@ -7,7 +8,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     img.onclick = function(){
         modal.style.display = "block";
         modalImg.src = this.src; // Get the source from the img tag
-        captionText.innerHTML = this.alt;
+        captionText.innerHTML = title.innerHTML;
     }
     // Get the span element that closes the modal
     modalImg.onclick = function(){
