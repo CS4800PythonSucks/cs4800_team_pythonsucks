269929364c732f839bf8221ec636eef5908e8168
hlin91 <hlin91@outlook.com>
2020-09-30 22:25:35 -0700

    Switched to jQuery syntax

--- app/javascript/packs/post_index.js
+++ app/javascript/packs/post_index.js
@@ -1,17 +1,12 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    var title = document.getElementById("imgTitle")
-    var modal = document.getElementById("homeModal");
-    var img = document.getElementById("homeImg");
-    var modalImg = document.getElementById("modalImg");
-    var captionText = document.getElementById("caption");
-    var span = document.getElementById("close");
-    img.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src; // Get the source from the img tag
-        captionText.innerHTML = title.innerHTML;
-    }
-    // Get the span element that closes the modal
-    modalImg.onclick = function(){
-        modal.style.display = "none";
-    }
-})
+$(document).ready(function() {
+    // Show modal when img is clicked
+    $("#homeImg").click(function() {
+        $("#homeModal").css("display", "block");
+        $("#modalImg").attr("src", $(this).attr("src"));
+        $("#caption").html($("#imgTitle").html());
+    });
+    // Hide modal when modal image is clicked
+    $("#modalImg").click(function() {
+        $("#homeModal").hide();
+    });
+});
