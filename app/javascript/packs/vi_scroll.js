640ee81cd4b315e380c2806e3f68212343108e20
hlin91 <hlin91@outlook.com>
2020-10-01 16:21:40 -0700

    Added vi-style page scrolling

---
+++ app/javascript/packs/vi_scroll.js
@@ -0,0 +1,13 @@
+// Vi style scrolling for webpage
+$(document).ready(function() {
+    const scrollStep = 1; // Number of pixels for a single 
+    const keyAction = {
+        j: $(window).scrollBy(0, scrollStep), // Scroll down
+        k: $(window).scrollBy(0, -scrollStep) // Scroll up
+    };
+    $(window).keypress(function(e) {
+        var key = e.which;
+        if (!(key in keyAction)) return;
+        keyAction[key]();
+    });
+});
