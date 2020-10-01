432528ef539989cbf1ecad53c5b7464a455a1986
hlin91 <hlin91@outlook.com>
2020-09-30 21:57:48 -0700

    Moved url logic from controller to model and added timestamp tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+  def is_video
+    if url.start_with? "https://www.youtube.com/" then
+      return true
+    elsif url.start_with? "https://v.redd.it/" then
+      return true
+    end
+    return false
+  end
+
+  def link
+    if url.start_with? "https://www.youtube.com/" then
+      return "https://www.youtube.com/embed/" + url[-11..-1]
+    elsif url.start_with? "https://v.redd.it/" then
+      return "https://www.vrddit.com/?v=www.reddit.com%2Fr%2F" + subreddit + "%2Fcomments%2F" + sub_id
+    else
+      return url
+    end
+  end
+
+  def source
+    return "https://redd.it/" + sub_id
+  end
+
+  def timestamp
+    t = Time.at(created)
+    return t.strftime("%x")
+  end
 end
