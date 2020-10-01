58e5e3cafd4d0e200f90458b1845f8dbcfff24f6
hlin91 <hlin91@outlook.com>
2020-09-30 22:00:29 -0700

    Removed url logic from controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,5 @@
 class PostsController < ApplicationController
   def index
     @post = Post.order("RAND()").first
-    @is_video = false
-    @url = @post.url
-    if @url.start_with? "https://www.youtube.com/" then
-      @is_video = true
-      @url = "https://www.youtube.com/embed/" + @post.url[-11..-1]
-    elsif @url.start_with? "https://v.redd.it/" then
-      @is_video = true
-      @url = "https://www.vrddit.com/?v=www.reddit.com%2Fr%2F" + @post.subreddit + "%2Fcomments%2F" + @post.sub_id
-    end
-    @link = "https://redd.it/" + @post.sub_id
   end
 end
