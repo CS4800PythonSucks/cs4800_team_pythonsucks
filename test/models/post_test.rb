4f1cb9237128f58389a2623598e916d12716ed8d
hlin91 <hlin91@outlook.com>
2020-10-06 00:00:47 -0700

    Added validation test for posts model for Assignment 6

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,11 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # Check that posts with no sub_id are invalid
+  test "invalid without sub_id" do
+    p = Post.new(url: "www.phonyurl.com")
+    refute p.valid?, "post is valid without a sub_id"
+    assert_not_nil user.errors[:url], "no validation error for url present"
+  end
 end
