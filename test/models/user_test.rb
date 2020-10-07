a060a974831da1fba27447ba7d44ca6121893eb7
seannakama <nakamasean@gmail.com>
2020-10-07 11:54:13 -0700

    Assignment 6 Test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+    #check that users have email present
+  	test 'invalid email password' do
+  		user = User.new(email: 'fake@example.com', encrypted_password: 'fakeperson1')
+  		assert_not_nil user.errors[:email], "no email present"
+  	end
 end
