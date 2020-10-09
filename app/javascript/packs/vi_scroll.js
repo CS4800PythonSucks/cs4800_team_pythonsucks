850a0ad2425176757d10ef6371d260d19b1c8d91
hlin91 <hlin91@outlook.com>
2020-10-08 22:44:55 -0700

    Fixed scrolling

--- app/javascript/packs/vi_scroll.js
+++ app/javascript/packs/vi_scroll.js
@@ -1,13 +1,32 @@
 // Vi style scrolling for webpage
 $(document).ready(function() {
-    const scrollStep = 1; // Number of pixels for a single 
-    const keyAction = {
-        j: window.scrollBy(0, scrollStep), // Scroll down
-        k: window.scrollBy(0, -scrollStep) // Scroll up
-    };
-    $(window).keypress(function(e) {
-        var key = e.which;
-        if (!(key in keyAction)) return;
-        keyAction[key]();
+    const scrollStep = 50; // Number of pixels for a single 
+    var lastScroll = 0; // Time of last scroll handled
+    $(window).keydown(function(e) { // Scroll page down on j and up on k
+        if (e.defaultPrevented) {
+            return;
+        }
+        var now = Date.now();
+        if (now - lastScroll > 10) {
+            switch (e.key) {
+            case "j":
+                window.scrollBy({
+                    top: scrollStep,
+                    left: 0
+                });
+                lastScroll = now;
+                break;
+            case "k":
+                window.scrollBy({
+                    top: -scrollStep,
+                    left: 0
+                });
+                lastScroll = now;
+                break;
+            default:
+                return;
+            }
+        }
+        e.preventDefault();
     });
 });
