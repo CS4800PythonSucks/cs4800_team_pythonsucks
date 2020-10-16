fda7aa12910edd22f72779d08ccc84d3663cd070
hlin91 <hlin91@outlook.com>
2020-10-15 18:24:22 -0700

    Changed styling and added drop down menus to gallery page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,25 +8,25 @@ class PostsController < ApplicationController
     params.require(:page)
     params.require(:sort)
     if params[:sort] == "new" then # Sort by created
-      if params[:source] != nil then
-        @posts = Post.paginate(page: params[:page], per_page: perPage).order("created DESC").where("broken = ? AND subreddit = ?", [false, params[:source]])
+      if params[:source] != nil and params[:source] != "all" then
+        @posts = Post.paginate(page: params[:page], per_page: perPage).order("created DESC").where("broken = ? AND subreddit = ?", false, params[:source])
       else
         @posts = Post.paginate(page: params[:page], per_page: perPage).order("created DESC").where(broken: false)
       end
     elsif params[:sort] == "votes" then # Sort by votes
-      if params[:source] != nil then
-        @posts = Post.paginate(page: params[:page], per_page: perPage).order("votes DESC").where("broken = ? AND subreddit = ?", [false, params[:source]])
+      if params[:source] != nil and params[:source] != "all" then
+        @posts = Post.paginate(page: params[:page], per_page: perPage).order("votes DESC").where("broken = ? AND subreddit = ?", false, params[:source])
       else
         @posts = Post.paginate(page: params[:page], per_page: perPage).order("votes DESC").where(broken: false)
       end
     elsif params[:sort] == "rand" then # Random order
-      if params[:source] != nil then
-        @posts = Post.paginate(page: params[:page], per_page: perPage).order("RAND()").where("broken = ? AND subreddit = ?", [false, params[:source]])
+      if params[:source] != nil and params[:source] != "all" then
+        @posts = Post.paginate(page: params[:page], per_page: perPage).order("RAND()").where("broken = ? AND subreddit = ?", false, params[:source])
       else
         @posts = Post.paginate(page: params[:page], per_page: perPage).order("RAND()").where(broken: false)
       end
     else # Fall back to sort by created
-      if params[:source] != nil then
+      if params[:source] != nil and params[:source] != "all" then
         @posts = Post.paginate(page: params[:page], per_page: perPage).order("created DESC").where("broken = ? AND subreddit = ?", [false, params[:source]])
       else
         @posts = Post.paginate(page: params[:page], per_page: perPage).order("created DESC").where(broken: false)
