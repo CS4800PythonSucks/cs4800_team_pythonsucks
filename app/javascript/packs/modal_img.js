14576b73f2f2d4fe5c1ab3d7a521fab46dcac9e8
hlin91 <hlin91@outlook.com>
2020-10-16 09:36:11 -0700

    Removed underscores from modal caption

--- app/javascript/packs/modal_img.js
+++ app/javascript/packs/modal_img.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     // Show modal when img is clicked
-    $("img").click(function() {
+    $("#postImg").click(function() {
         $("#modalDiv").css("display", "block");
         $("#modalImg").attr("src", $(this).attr("src"));
-        $("#caption").text($(this).data("title"));
+        $("#caption").text($(this).data("title").replace(/_/g, " "));
     });
     // Hide modal when modal image is clicked
     $("#modalImg").click(function() {
